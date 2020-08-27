8450d51ebd4bec23d89ab3c7c2c6b915b624b73e
Yisroel Newmark <ymnewmark@gmail.com>
2020-08-27 12:36:09 -0400

    Some changes

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+top = tkinter.Tk()
+
+var = StringVar()
+label = Label( root, textvariable = var, relief = RAISED )
+
+var.set("Hey!? How are you doing?")
+label.pack()
+
+top.mainloop()
+
