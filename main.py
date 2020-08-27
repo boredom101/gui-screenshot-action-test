0de0fe05d55ada3508525fc267fe874796ff7879
boredom101 <ymnewmark@gmail.com>
2020-08-27 15:15:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import tkinter
 top = tkinter.Tk()
 
 var = tkinter.StringVar()
-label = tkinter.Label( root, textvariable = var, relief = tkinter.RAISED )
+label = tkinter.Label( top, textvariable = var, relief = tkinter.RAISED )
 
 var.set("Hey!? How are you doing?")
 label.pack()
